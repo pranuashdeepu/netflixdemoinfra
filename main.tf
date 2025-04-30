8ba5f57284d7b47cc414c87b919179b39864377b
pranuashdeepu <praneethreddynallapareddy2588@gmail.com>
2025-04-30 10:42:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,10 +4,10 @@ region = "ap-south-1"
 
 resource "aws_instance" "one" {
 count = 4
-ami = "ami-076c6dbba59aa92e6"
+ami = "ami-091dccf4e2d272bae"
 instance_type = "t2.medium"
 key_name = "pranu"
-vpc_security_group_ids = ["sg-078550f19e8ad166e"]
+vpc_security_group_ids = ["sg-02b0309be0f635b9e"]
 tags = {
 Name = var.instance_names[count.index]
 }
