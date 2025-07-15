3eed4885750da8895d318bf50a67df28b7517d7c
pranuashdeepu <praneethreddynallapareddy2588@gmail.com>
2025-07-15 18:37:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@ region = "ap-south-1"
 }
 
 resource "aws_instance" "one" {
-count = 4
-ami = "ami-0d03cb826412c6b0f"
+count = 6
+ami = "ami-0a1235697f4afa8a4"
 instance_type = "t2.medium"
 key_name = "pranukeypair"
 vpc_security_group_ids = ["sg-02bedbd208d6dc4f8"]
@@ -14,5 +14,5 @@ Name = var.instance_names[count.index]
 }
 
 variable "instance_names" {
-default = ["jenkins", "AppServer-1", "AppServer-2", "Monitoring server"]
+default = ["jenkins", "AppServer-1", "AppServer-2", "Monitoring server","test-1","test-2"]
 }
